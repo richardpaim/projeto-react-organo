a30a2ac2a131e02b8d9abb025b12447d40a80e8c
Richard Ribebiro <richard.diass@hotmail.com>
2023-09-30 10:43:53 -0300

    finalizado projeto organo com ReactJS

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,10 @@
+import './Banner.css';
+function Banner () {
+    return (
+        <header className="banner">
+        <img src="/images/banner.png" alt="Imagem do Banner"></img>
+        </header>
+    )
+}
+
+export default Banner
